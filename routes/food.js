7e21bb3b0079f7e4b06f2905b5afde47e75fe666
Rohith-Chittipolu <98173410+Rohith-Chittipolu@users.noreply.github.com>
2022-04-24 17:13:44 -0500

    Update food.js

--- routes/food.js
+++ routes/food.js
@@ -1,26 +1,25 @@
 var express = require('express');
 const food_controlers= require('../controllers/food');
 var router = express.Router();
-
+// A little function to check if we have an authorized user and continue on or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+  if (req.user){ 
+    return next(); 
+  } 
+  req.session.returnTo = req.originalUrl; 
+  res.redirect("/login"); 
+} 
 /* GET home page. */
  router.get('/', food_controlers.food_view_all_Page );
 /* GET detail food page */ 
 router.get('/detail', food_controlers.food_view_one_Page); 
 /* GET create food page */ 
-router.get('/create',food_controlers.food_create_Page);
+router.get('/create',secured,food_controlers.food_create_Page);
 /* GET create update page */ 
-// A little function to check if we have an authorized user and continue on or 
-// redirect to login. 
-const secured = (req, res, next) => { 
-    if (req.user){ 
-      return next(); 
-    } 
-    req.session.returnTo = req.originalUrl; 
-    res.redirect("/login"); 
-  } 
 router.get('/update',secured, food_controlers.food_update_Page); 
 /* GET delete food page */ 
-router.get('/delete', food_controlers.food_delete_Page);
+router.get('/delete',secured, food_controlers.food_delete_Page);
  
 
 module.exports = router;
